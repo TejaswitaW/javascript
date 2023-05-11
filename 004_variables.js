4999102a658bc17b3aa7b712c294d37ecefb3bc0
tejaswitaw <tejaswitawakhure@gmail.com>
2023-05-11 16:38:31 +0530

    const use

---
+++ 004_variables.js
@@ -0,0 +1,6 @@
+// at the time of declaration of variable you have to 
+// assign value otherwise you will get error
+const city = "Berlin";
+console.log(city)
+// following line give error
+// const name;
