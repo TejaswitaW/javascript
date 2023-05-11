32b366c39182f01a095bb1b66082f04ad0f51e7e
tejaswitaw <tejaswitawakhure@gmail.com>
2023-05-11 16:03:33 +0530

    let ,const variable declaration

---
+++ 002_variables.js
@@ -0,0 +1,11 @@
+let city = "Pune";
+const pi = 3.14;
+console.log(city);
+console.log(pi);
+
+city = "Bengaluru";
+console.log(city);
+// following line gives error because 
+// we cant change variable decalred with const
+/*pi = 8.90;
+console.log(pi)*/
