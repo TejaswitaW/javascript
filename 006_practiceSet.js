90aefbbf89b37956a5b0d798840eb9c6491d0ed6
tejaswitaw <tejaswitawakhure@gmail.com>
2023-05-11 17:29:39 +0530

    practice questions

---
+++ 006_practiceSet.js
@@ -0,0 +1,24 @@
+// create variable with string data and try to add number
+let city = "Berlin";
+console.log(city);
+let newCity = city + 1804;
+console.log(newCity);
+
+const person = {
+    name:"Ram",
+    age:22,
+    city:"Bengaluru"
+};
+console.log(person);
+// const object we can modify , but cannot reasign
+person["address"] = "HSR";
+console.log(person)
+
+// word ,meaning dictionary
+const dictionary = {
+    assault:"attack",
+    awful:"Very bad or unpleasant"
+}
+
+console.log(dictionary)
+console.log(dictionary.assault)
