159ad0a8db7cd3d50dba0d8e2261d94105393279
tejaswitaw <tejaswitawakhure@gmail.com>
2023-05-11 21:24:59 +0530

    operators and expressions

---
+++ 007_operatorsExpressions.js
@@ -0,0 +1,35 @@
+// operators
+let a = 18;
+let t = 4;
+console.log("Addition: ",a+t);
+console.log("Subtraction: ",a-t)
+console.log("Multiplication: ",a*t)
+console.log("Division: ",a/t)
+console.log("Modulo: ",a%t)
+console.log("Exponentiation: ",a**t)
+console.log(a++)
+console.log(a)
+console.log(++t)
+let z = 1;
+console.log(z--)
+console.log(z)
+console.log(--z)
+let x = 5;
+console.log(x);
+// x += 5;
+// console.log(x);
+let p = "5";
+console.log(x == p)// without checking type
+console.log(x === p)// strict type checking
+//logical
+let m = 100;
+let n = 200;
+//and
+console.log(m === 100 && n === 200)
+//or
+let k = 90;
+let l = 100;
+console.log(k === 0 || l === 5)
+//not
+console.log(!k)
+console.log(!0)
